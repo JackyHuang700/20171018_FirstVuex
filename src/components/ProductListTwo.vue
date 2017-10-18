025671b580ee2ae03a962bf2ff90bc4fe551ceb0
Jacky <Jacky>
2017-10-18 15:52:20 +0800

    project init

---
+++ src/components/ProductListTwo.vue
@@ -0,0 +1,54 @@
+<template>
+    <div id="product-list-two">
+    <h2>product-list-two</h2>
+    <ul>
+      <li v-for='product in productss'>
+        <span class="name">{{product.name}}</span>
+        <span class="price">£{{product.price}}</span>
+      </li>
+    </ul>
+    </div>
+</template>
+
+<script>
+
+import { mapGetters } from 'vuex'
+export default{
+    // name: '',
+  // props: ['productss'],
+  data () {
+    return {
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'productss'
+    ])
+  }
+}
+</script>
+
+
+<style scoped>
+#product-list-two{
+    background: #D1E4FF;
+    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
+    margin-bottom: 30px;
+    padding: 10px 20px;
+}
+#product-list-two ul{
+    padding: 0;
+    list-style-type: none;
+}
+#product-list-two li{
+    margin-right: 10px;
+    margin-top: 10px;
+    padding: 20px;
+    background: rgba(255,255,255,0.7);
+}
+.price{
+    font-weight: bold;
+    color: #860CE8;
+    display: block;
+}
+</style>
