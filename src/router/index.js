4fba2ba32add4918988ae8c68445e63be773dc8b
Jacky <Jacky>
2017-10-18 16:48:44 +0800

    fix path

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import App from '@/App'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
+    },
+    {
+      path: '/20171018_FirstVuex',
+      name: '20171018_FirstVuex',
+      component: App
     }
   ]
 })
