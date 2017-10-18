9bef301a358f972edfd14bf55b911467f1010db8
Jacky <Jacky>
2017-10-18 16:54:06 +0800

    fix 多餘的router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import App from '@/App'
 
 Vue.use(Router)
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: HelloWorld
-    },
-    {
-      path: '/20171018_FirstVuex',
-      name: '20171018_FirstVuex',
-      component: App
     }
   ]
 })
